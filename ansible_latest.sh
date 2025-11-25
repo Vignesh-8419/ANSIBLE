b186de66e0a2edbbdddae5cd45d77c2d4938ba14
Vignesh-8419 <vvekar6@gmail.com>
2025-11-26 04:04:46 +0530

    Update ansible_latest.sh

--- ansible_latest.sh
+++ ansible_latest.sh
@@ -115,3 +115,18 @@ echo "✅ AWX installation complete. Access it via http://localhost or your serv
 #sed -i 's/kwargs = kwargs_from_env(environment=environment, ssl_version=tls_version)/kwargs = kwargs_from_env(environment=environment)/' \
   /usr/local/lib/python3.9/site-packages/compose/cli/docker_client.py
 #requests==2.31.0 
+
+
+[root@test-pxe01 installer]# cat inventory | head
+localhost ansible_connection=local ansible_python_interpreter="/usr/bin/env python3"
+
+[all:vars]
+ansible_python_interpreter=/usr/bin/python3.9
+
+# Remove these lines if you want to run a local image build
+# Otherwise the setup playbook will install the official Ansible images. Versions may
+# be selected based on: latest, 1, 1.0, 1.0.0, 1.0.0.123
+# by default the base will be used to search for ansible/awx
+dockerhub_base=ansible
+[root@test-pxe01 installer]#
+
