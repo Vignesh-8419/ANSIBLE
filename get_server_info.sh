c324289d36af0512eaa459f1f8024d74b7b62030
Vignesh-8419 <vvekar6@gmail.com>
2026-01-10 04:44:39 +0530

    Update get_server_info.sh

--- get_server_info.sh
+++ get_server_info.sh
@@ -2,24 +2,27 @@
 
 USER="root"
 PASS="Root@123"
+SERVER_FILE="servers.txt"
+PARALLEL=20
 
-if [ -z "$1" ]; then
-  echo "Usage: $0 <server_ip>"
+if [ ! -f "$SERVER_FILE" ]; then
+  echo "servers.txt not found!"
   exit 1
 fi
 
+run_server() {
 SERVER=$1
 
-sshpass -p "$PASS" ssh -o StrictHostKeyChecking=no $USER@$SERVER << EOF
+sshpass -p "$PASS" ssh -tt -o StrictHostKeyChecking=no -o ConnectTimeout=5 $USER@$SERVER << EOF
 echo "SERVER: $SERVER"
-
 echo "-----------------------------------------"
+
 echo "Hostname:"
 hostname
 echo
 
 echo "OS Release:"
-cat /etc/os-release | grep PRETTY_NAME | cut -d= -f2 | tr -d '"'
+grep PRETTY_NAME /etc/os-release | cut -d= -f2 | tr -d '"'
 echo
 
 echo "Kernel Version:"
@@ -29,11 +32,17 @@ echo
 echo "Interface | IP Address | MAC Address"
 echo "------------------------------------"
 
-ip -o -4 addr show up | awk '{print $2, $4}' | while read iface ip; do
-    mac=$(cat /sys/class/net/$iface/address)
-    printf "%s | %s | %s\n" "$iface" "$ip" "$mac"
+ip -o -4 addr show up | awk '{print \$2, \$4}' | while read iface ip; do
+    mac=\$(cat /sys/class/net/\$iface/address)
+    printf "%s | %s | %s\n" "\$iface" "\$ip" "\$mac"
 done
 
 echo "-----------------------------------------"
-
 EOF
+}
+
+export -f run_server
+export USER PASS
+
+cat "$SERVER_FILE" | xargs -n 1 -P $PARALLEL bash -c 'run_server "$@"' _
+
