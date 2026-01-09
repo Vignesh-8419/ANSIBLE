9d8e7582945b4852e4a4d968b0b2f44560b87fea
Vignesh-8419 <vvekar6@gmail.com>
2026-01-10 04:37:35 +0530

    Create get_server_info.sh

---
+++ get_server_info.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+USER="root"
+PASS="Root@123"
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <server_ip>"
+  exit 1
+fi
+
+SERVER=$1
+
+sshpass -p "$PASS" ssh -o StrictHostKeyChecking=no $USER@$SERVER << 'EOF'
+
+echo "-----------------------------------------"
+echo "Hostname:"
+hostname
+echo
+
+echo "OS Release:"
+cat /etc/os-release | grep PRETTY_NAME | cut -d= -f2 | tr -d '"'
+echo
+
+echo "Kernel Version:"
+uname -r
+echo
+
+echo "Interface | IP Address | MAC Address"
+echo "------------------------------------"
+
+ip -o -4 addr show up | awk '{print $2, $4}' | while read iface ip; do
+    mac=$(cat /sys/class/net/$iface/address)
+    printf "%s | %s | %s\n" "$iface" "$ip" "$mac"
+done
+
+echo "-----------------------------------------"
+
+EOF
