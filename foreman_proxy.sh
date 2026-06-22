cf5e100c4d065e9c215b6866394664083deb2767
Vignesh-8419 <vvekar6@gmail.com>
2026-06-23 00:55:38 +0530

    Update foreman_proxy.sh

--- foreman_proxy.sh
+++ foreman_proxy.sh
@@ -145,20 +145,20 @@ firewall-cmd --add-port=443/tcp --permanent
 firewall-cmd --reload
 
 echo "🚀 Running Foreman Proxy installer..."
-foreman-installer \\
-  --scenario foreman-proxy-content \\
-  --certs-tar-file "/root/${FOREMAN_PROXY}-certs.tar" \\
-  --foreman-proxy-register-in-foreman "true" \\
-  --foreman-proxy-foreman-base-url "https://cent-07-01.vgs.com" \\
-  --foreman-proxy-trusted-hosts "cent-07-01.vgs.com" \\
-  --foreman-proxy-trusted-hosts "${FOREMAN_PROXY}" \\
-  --foreman-proxy-oauth-consumer-key "${OAUTH_KEY}" \\
-  --foreman-proxy-oauth-consumer-secret "${OAUTH_SECRET}" \\
-  --foreman-proxy-dhcp true \\
-  --foreman-proxy-dhcp-interface "\$INTERFACE" \\
-  --foreman-proxy-dns true \\
-  --foreman-proxy-dns-interface "\$INTERFACE" \\
-  --foreman-proxy-tftp true \\
+foreman-installer \
+  --scenario foreman-proxy-content \
+  --certs-tar-file "/root/${FOREMAN_PROXY}-certs.tar" \
+  --foreman-proxy-register-in-foreman "true" \
+  --foreman-proxy-foreman-base-url "https://cent-07-01.vgs.com" \
+  --foreman-proxy-trusted-hosts "cent-07-01.vgs.com" \
+  --foreman-proxy-trusted-hosts "${FOREMAN_PROXY}" \
+  --foreman-proxy-oauth-consumer-key "${OAUTH_KEY}" \
+  --foreman-proxy-oauth-consumer-secret "${OAUTH_SECRET}" \
+  --foreman-proxy-dhcp true \
+  --foreman-proxy-dhcp-interface "\$INTERFACE" \
+  --foreman-proxy-dns true \
+  --foreman-proxy-dns-interface "\$INTERFACE" \
+  --foreman-proxy-tftp true \
   --foreman-proxy-tftp-servername "${FOREMAN_PROXY}"
 
 
