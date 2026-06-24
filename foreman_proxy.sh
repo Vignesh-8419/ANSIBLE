b16610dc21fc8e3de6b9f9d5b4987e83c60c311c
Vignesh-8419 <vvekar6@gmail.com>
2026-06-24 19:29:44 +0530

    Update foreman_proxy.sh

--- foreman_proxy.sh
+++ foreman_proxy.sh
@@ -137,12 +137,15 @@ firewall-cmd --reload
 
 echo "🔧 Updating /etc/hosts entries ..."
 
-NEW_HOSTNAME=$(hostname -s)
+NEW_HOSTNAME=$(hostname -s | tr -d '[:space:]')
 JUST_IP=$(hostname -I | awk '{print $1}')
 
 echo "Updating /etc/hosts file..."
 
-sed -i "/${NEW_HOSTNAME}/d" /etc/hosts
+if [[ -n "$NEW_HOSTNAME" ]]; then
+    sed -i "\|${NEW_HOSTNAME}|d" /etc/hosts
+fi
+
 echo "${JUST_IP} ${NEW_HOSTNAME}.vgs.com ${NEW_HOSTNAME}" >> /etc/hosts
 
 grep "${NEW_HOSTNAME}.vgs.com" /etc/hosts
