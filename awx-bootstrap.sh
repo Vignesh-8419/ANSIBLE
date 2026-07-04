bf1b12eb9df4a6f6d05d0a5cd31c3fb72ee1dc1d
Vignesh-8419 <vvekar6@gmail.com>
2026-07-04 20:26:06 +0530

    Update awx-bootstrap.sh

--- awx-bootstrap.sh
+++ awx-bootstrap.sh
@@ -102,6 +102,17 @@ kubectl exec -i "$POD" \
 
 set -e
 
+# Console Colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+CYAN='\033[0;36m'
+MAGENTA='\033[0;35m'
+WHITE='\033[1;37m'
+BOLD='\033[1m'
+NC='\033[0m'
+
 echo
 echo "Running AWX Bootstrap..."
 echo
