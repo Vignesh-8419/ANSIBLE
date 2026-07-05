86a7f5e66b2bfdd583d5512cd4136164bae811c6
Vignesh-8419 <vvekar6@gmail.com>
2026-07-06 00:33:35 +0530

    Update foreman_proxy.sh

--- foreman_proxy.sh
+++ foreman_proxy.sh
@@ -191,8 +191,8 @@ systemctl restart dhcpd
 systemctl enable dhcpd
 
 
-sshpass -p 'Vigneshv12$' scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@netbox.vgs.com:/boot/efi/EFI/rocky/shimx64.efi /var/lib/tftpboot/grub2/
-sshpass -p 'Vigneshv12$' scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@netbox.vgs.com:/boot/efi/EFI/rocky/grub.cfg /var/lib/tftpboot/grub2/
+sshpass -p 'Vigneshv12$' scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null admin@netbox.vgs.com:/boot/efi/EFI/rocky/shimx64.efi /var/lib/tftpboot/grub2/
+sshpass -p 'Vigneshv12$' scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null admin@netbox.vgs.com:/boot/efi/EFI/rocky/grub.cfg /var/lib/tftpboot/grub2/
 
 
 # ======================================================
