c7515ee67ea3373543b2d8942f9357482463906e
Vignesh-8419 <vvekar6@gmail.com>
2026-08-08 03:55:29 +0530

    Update get_foreman_ids.sh

--- get_foreman_ids.sh
+++ get_foreman_ids.sh
@@ -1,256 +1,156 @@
 #!/bin/bash
-
 ###############################################################################
 # Foreman Dynamic ID Lookup
 #
-# Supports:
+# Generates Ansible variables for:
 #
-# RAID:
-#   CentOS Linux 7 RAID
-#   Rocky Linux 8.10 RAID
-#   Rocky Linux 9.2 RAID
-#   Rocky Linux 9.8 RAID
+# hostgroup:
+#   1 = CentOS Linux 7
+#   2 = Rocky Linux 8.10
+#   3 = Rocky Linux 9.2
+#   4 = Rocky Linux 9.8
 #
-# Single Disk:
-#   CentOS Linux 7 SingleDisk
-#   Rocky Linux 8.10 SingleDisk
-#   Rocky Linux 9.2 SingleDisk
-#   Rocky Linux 9.8 SingleDisk
+# disk_layout:
+#   raid
+#   single
 #
 ###############################################################################
 
-
 FOREMAN_SERVER="https://cent-07-01.vgs.com"
-
 FOREMAN_USER="admin"
-
 FOREMAN_PASS="zqs977dXzqfEvTML"
 
 
+###############################################################################
+# Foreman Hostgroups
+###############################################################################
+
+declare -A HOSTGROUP_NAMES
+
+HOSTGROUP_NAMES["1"]="CentOSLinux 7 RAID"
+HOSTGROUP_NAMES["2"]="RockyLinux 8.10 RAID"
+HOSTGROUP_NAMES["3"]="RockyLinux 9.2 RAID"
+HOSTGROUP_NAMES["4"]="RockyLinux 9.8 RAID"
+
 
 ###############################################################################
-# Hostgroup Mapping
+# Storage Hostgroup Mapping
 ###############################################################################
 
-declare -A HOSTGROUP_MAP
+declare -A RAID_HOSTGROUP_IDS
+
+RAID_HOSTGROUP_IDS["1"]=5
+RAID_HOSTGROUP_IDS["2"]=6
+RAID_HOSTGROUP_IDS["3"]=8
+RAID_HOSTGROUP_IDS["4"]=7
 
 
-HOSTGROUP_MAP["CentOSLinux 7 RAID"]="1"
-HOSTGROUP_MAP["RockyLinux 8.10 RAID"]="2"
-HOSTGROUP_MAP["RockyLinux 9.2 RAID"]="3"
-HOSTGROUP_MAP["RockyLinux 9.8 RAID"]="4"
+declare -A SINGLE_HOSTGROUP_IDS
+
+SINGLE_HOSTGROUP_IDS["1"]=9
+SINGLE_HOSTGROUP_IDS["2"]=10
+SINGLE_HOSTGROUP_IDS["3"]=11
+SINGLE_HOSTGROUP_IDS["4"]=12
 
 
-HOSTGROUP_MAP["CentOSLinux 7 SingleDisk"]="5"
-HOSTGROUP_MAP["RockyLinux 8.10 SingleDisk"]="6"
-HOSTGROUP_MAP["RockyLinux 9.2 SingleDisk"]="7"
-HOSTGROUP_MAP["RockyLinux 9.8 SingleDisk"]="8"
-
-
-
-declare -A HOSTGROUP_IDS
+###############################################################################
+# Dynamic Lookup
+###############################################################################
 
 declare -A OS_IDS
-
 declare -A MEDIUM_IDS
 
-declare -A PTABLE_IDS
 
-
-
-HOSTGROUPS=(
-
-"CentOSLinux 7 RAID"
-
-"RockyLinux 8.10 RAID"
-
-"RockyLinux 9.2 RAID"
-
-"RockyLinux 9.8 RAID"
-
-
-"CentOSLinux 7 SingleDisk"
-
-"RockyLinux 8.10 SingleDisk"
-
-"RockyLinux 9.2 SingleDisk"
-
-"RockyLinux 9.8 SingleDisk"
-
-)
-
-
-
-ARCH_ID=""
-
-
-
-###############################################################################
-# Lookup IDs
-###############################################################################
-
-for HG in "${HOSTGROUPS[@]}"
+for IDX in 1 2 3 4
 do
 
+    HG="${HOSTGROUP_NAMES[$IDX]}"
 
-echo "Checking Hostgroup : ${HG}"
-
-
-INFO=$(hammer \
-    --server "$FOREMAN_SERVER" \
-    --username "$FOREMAN_USER" \
-    --password "$FOREMAN_PASS" \
-    hostgroup info \
-    --name "$HG" 2>/dev/null)
+    echo
+    echo "Checking Hostgroup : ${HG}"
 
 
-
-IDX=${HOSTGROUP_MAP[$HG]}
-
-
-
-HOSTGROUP_IDS[$IDX]=$(echo "$INFO" |
-awk -F': *' '/^Id:/ {print $2}')
-
+    INFO=$(hammer \
+        --server "$FOREMAN_SERVER" \
+        --username "$FOREMAN_USER" \
+        --password "$FOREMAN_PASS" \
+        hostgroup info \
+        --name "$HG" 2>/dev/null)
 
 
-OS_NAME=$(echo "$INFO" |
-awk -F': *' '/Operating System:/ {print $2}')
+    OS_NAME=$(echo "$INFO" | awk -F': *' '/Operating System:/ {print $2}')
+    MEDIUM_NAME=$(echo "$INFO" | awk -F': *' '/Medium:/ {print $2}')
 
 
+    OS_IDS[$IDX]=$(
+        hammer \
+        --server "$FOREMAN_SERVER" \
+        --username "$FOREMAN_USER" \
+        --password "$FOREMAN_PASS" \
+        os list |
+        awk -F'|' -v os="$OS_NAME" '
+        {
+            gsub(/^ +| +$/, "", $1)
+            gsub(/^ +| +$/, "", $2)
 
-MEDIUM_NAME=$(echo "$INFO" |
-awk -F': *' '/Medium:/ {print $2}')
-
-
-
-PTABLE_NAME=$(echo "$INFO" |
-awk -F': *' '/Partition Table:/ {print $2}')
-
-
-
-ARCH_NAME=$(echo "$INFO" |
-awk -F': *' '/Architecture:/ {print $2}')
-
-
-
-OS_IDS[$IDX]=$(
-hammer \
---server "$FOREMAN_SERVER" \
---username "$FOREMAN_USER" \
---password "$FOREMAN_PASS" \
-os list |
-awk -F'|' -v os="$OS_NAME" '
-
-{
-gsub(/^ +| +$/,"",$1)
-gsub(/^ +| +$/,"",$2)
-
-if ($2==os)
-print $1
-
-}
-'
-)
+            if($2==os)
+                print $1
+        }'
+    )
 
 
+    MEDIUM_IDS[$IDX]=$(
+        hammer \
+        --server "$FOREMAN_SERVER" \
+        --username "$FOREMAN_USER" \
+        --password "$FOREMAN_PASS" \
+        medium list |
+        awk -F'|' -v m="$MEDIUM_NAME" '
+        {
+            gsub(/^ +| +$/, "", $1)
+            gsub(/^ +| +$/, "", $2)
 
-MEDIUM_IDS[$IDX]=$(
-hammer \
---server "$FOREMAN_SERVER" \
---username "$FOREMAN_USER" \
---password "$FOREMAN_PASS" \
-medium list |
-awk -F'|' -v m="$MEDIUM_NAME" '
-
-{
-gsub(/^ +| +$/,"",$1)
-gsub(/^ +| +$/,"",$2)
-
-if ($2==m)
-print $1
-
-}
-'
-)
-
-
-
-PTABLE_IDS[$IDX]=$(
-hammer \
---server "$FOREMAN_SERVER" \
---username "$FOREMAN_USER" \
---password "$FOREMAN_PASS" \
-partition-table list |
-awk -F'|' -v p="$PTABLE_NAME" '
-
-{
-gsub(/^ +| +$/,"",$1)
-gsub(/^ +| +$/,"",$2)
-
-if ($2==p)
-print $1
-
-}
-'
-)
-
-
-
-if [[ -z "$ARCH_ID" ]]
-then
-
-ARCH_ID=$(
-hammer \
---server "$FOREMAN_SERVER" \
---username "$FOREMAN_USER" \
---password "$FOREMAN_PASS" \
-architecture list |
-awk -F'|' -v a="$ARCH_NAME" '
-
-{
-gsub(/^ +| +$/,"",$1)
-gsub(/^ +| +$/,"",$2)
-
-if ($2==a)
-print $1
-
-}
-'
-)
-
-fi
-
+            if($2==m)
+                print $1
+        }'
+    )
 
 done
 
 
 
 ###############################################################################
-# Generate Ansible Variables
+# Generate Ansible Mapping
 ###############################################################################
 
 cat <<EOF
 
 
-###############################################################################
+###########################################################################
 # Host Group
 #
-# 1 = CentOS Linux 7 RAID
-# 2 = Rocky Linux 8.10 RAID
-# 3 = Rocky Linux 9.2 RAID
-# 4 = Rocky Linux 9.8 RAID
+# 1 = CentOS Linux 7
+# 2 = Rocky Linux 8.10
+# 3 = Rocky Linux 9.2
+# 4 = Rocky Linux 9.8
 #
-# 5 = CentOS Linux 7 SingleDisk
-# 6 = Rocky Linux 8.10 SingleDisk
-# 7 = Rocky Linux 9.2 SingleDisk
-# 8 = Rocky Linux 9.8 SingleDisk
-#
-###############################################################################
+###########################################################################
 
 hostgroup: "{{ hostgroup | default('1', true) }}"
 
+disk_layout: "{{ disk_layout | default('raid', true) }}"
 
+
+###########################################################################
+# Subnet Mapping
+#
+# CentOS 7
+#   vgs-subnet-centos : 1
+#
+# Rocky Linux 8/9
+#   vgs-subnet-rockyos : 2
+#
+###########################################################################
 
 subnet_id: >-
   {{
@@ -258,35 +158,52 @@ subnet_id: >-
       '1': 1,
       '2': 2,
       '3': 2,
-      '4': 2,
-
-      '5': 1,
-      '6': 2,
-      '7': 2,
-      '8': 2
-
+      '4': 2
     }[hostgroup | string]
   }}
 
 
+###########################################################################
+# Hostgroup Mapping
+#
+# RAID
+#
+# 1-raid  CentOS Linux 7 RAID       ID:${RAID_HOSTGROUP_IDS[1]}
+# 2-raid  Rocky Linux 8.10 RAID     ID:${RAID_HOSTGROUP_IDS[2]}
+# 3-raid  Rocky Linux 9.2 RAID      ID:${RAID_HOSTGROUP_IDS[3]}
+# 4-raid  Rocky Linux 9.8 RAID      ID:${RAID_HOSTGROUP_IDS[4]}
+#
+#
+# SINGLE DISK
+#
+# 1-single CentOS Linux 7 SingleDisk    ID:${SINGLE_HOSTGROUP_IDS[1]}
+# 2-single Rocky Linux 8.10 SingleDisk  ID:${SINGLE_HOSTGROUP_IDS[2]}
+# 3-single Rocky Linux 9.2 SingleDisk   ID:${SINGLE_HOSTGROUP_IDS[3]}
+# 4-single Rocky Linux 9.8 SingleDisk   ID:${SINGLE_HOSTGROUP_IDS[4]}
+#
+###########################################################################
 
 hostgroup_id: >-
   {{
     {
-      '1': ${HOSTGROUP_IDS[1]},
-      '2': ${HOSTGROUP_IDS[2]},
-      '3': ${HOSTGROUP_IDS[3]},
-      '4': ${HOSTGROUP_IDS[4]},
+      '1-raid': ${RAID_HOSTGROUP_IDS[1]},
+      '2-raid': ${RAID_HOSTGROUP_IDS[2]},
+      '3-raid': ${RAID_HOSTGROUP_IDS[3]},
+      '4-raid': ${RAID_HOSTGROUP_IDS[4]},
 
-      '5': ${HOSTGROUP_IDS[5]},
-      '6': ${HOSTGROUP_IDS[6]},
-      '7': ${HOSTGROUP_IDS[7]},
-      '8': ${HOSTGROUP_IDS[8]}
+      '1-single': ${SINGLE_HOSTGROUP_IDS[1]},
+      '2-single': ${SINGLE_HOSTGROUP_IDS[2]},
+      '3-single': ${SINGLE_HOSTGROUP_IDS[3]},
+      '4-single': ${SINGLE_HOSTGROUP_IDS[4]}
 
-    }[hostgroup | string]
+    }[hostgroup ~ '-' ~ disk_layout]
   }}
 
 
+
+###########################################################################
+# Operating System Mapping
+###########################################################################
 
 operatingsystem_id: >-
   {{
@@ -294,17 +211,15 @@ operatingsystem_id: >-
       '1': ${OS_IDS[1]},
       '2': ${OS_IDS[2]},
       '3': ${OS_IDS[3]},
-      '4': ${OS_IDS[4]},
-
-      '5': ${OS_IDS[5]},
-      '6': ${OS_IDS[6]},
-      '7': ${OS_IDS[7]},
-      '8': ${OS_IDS[8]}
-
+      '4': ${OS_IDS[4]}
     }[hostgroup | string]
   }}
 
 
+
+###########################################################################
+# Installation Media Mapping
+###########################################################################
 
 medium_id: >-
   {{
@@ -312,38 +227,34 @@ medium_id: >-
       '1': ${MEDIUM_IDS[1]},
       '2': ${MEDIUM_IDS[2]},
       '3': ${MEDIUM_IDS[3]},
-      '4': ${MEDIUM_IDS[4]},
-
-      '5': ${MEDIUM_IDS[5]},
-      '6': ${MEDIUM_IDS[6]},
-      '7': ${MEDIUM_IDS[7]},
-      '8': ${MEDIUM_IDS[8]}
-
+      '4': ${MEDIUM_IDS[4]}
     }[hostgroup | string]
   }}
 
 
+
+###########################################################################
+# Partition Table Mapping
+#
+# RAID:
+#   RAID1 EFI + /boot RAID1 + LVM RAID1
+#
+# SINGLE:
+#   EFI + LVM Single Disk
+#
+###########################################################################
 
 ptable_id: >-
   {{
     {
-      '1': ${PTABLE_IDS[1]},
-      '2': ${PTABLE_IDS[2]},
-      '3': ${PTABLE_IDS[3]},
-      '4': ${PTABLE_IDS[4]},
-
-      '5': ${PTABLE_IDS[5]},
-      '6': ${PTABLE_IDS[6]},
-      '7': ${PTABLE_IDS[7]},
-      '8': ${PTABLE_IDS[8]}
-
-    }[hostgroup | string]
+      'raid': 126,
+      'single': 127
+    }[disk_layout]
   }}
 
 
 
-architecture_id: ${ARCH_ID}
-
+architecture_id: 1
 
 
 EOF
